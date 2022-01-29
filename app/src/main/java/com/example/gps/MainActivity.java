55706935fb25d493b5127ef25a40c7fa6c12a891
Chojar-alt <ml.chojaczyk+1@gmail.com>
2022-01-29 19:36:08 +0100

    Z bazą danych, zapisuje jedną wartoś w bazie i wyświetala
    TODO
    Zrobić drugą kolumnę i podzielić wyświetalnie danych
    ogarnąć interfejs
    Przesyłanie danych na server

--- app/src/main/java/com/example/gps/MainActivity.java
+++ app/src/main/java/com/example/gps/MainActivity.java
@@ -12,12 +12,20 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView textViewLatitude, textViewLongitude;
-    private LocationManager locationManager;
+    TextView textViewLatitude, textViewLongitude;
+    LocationManager locationManager;
+    ListView listView;
+    DatabaseHelper databaseHelper;
+    ArrayList arrayList;
+    ArrayAdapter arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +35,16 @@ public class MainActivity extends AppCompatActivity {
         textViewLatitude = findViewById(R.id.latitude);
         textViewLongitude = findViewById(R.id.longitude);
         locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        listView = findViewById(R.id.list_view);
+
+        databaseHelper = new DatabaseHelper(MainActivity.this);
+
+        arrayList = databaseHelper.getAllText();
+
+        arrayAdapter = new ArrayAdapter( MainActivity.this,
+                android.R.layout.simple_list_item_1, arrayList);
+
+        listView.setAdapter(arrayAdapter);
 
         if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION)!= PackageManager.PERMISSION_GRANTED &&
         ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
@@ -38,6 +56,18 @@ public class MainActivity extends AppCompatActivity {
             public void onLocationChanged(Location location) {
                 textViewLongitude.setText(String.valueOf(location.getLongitude()));
                 textViewLatitude.setText(String.valueOf(location.getLatitude()));
+                String text = textViewLatitude.getText().toString();
+                if(!text.isEmpty()){
+                    if (databaseHelper.addText(text)){
+
+                        textViewLatitude.setText("");
+                        arrayList.clear();
+                        arrayList.addAll(databaseHelper.getAllText());
+                        arrayAdapter.notifyDataSetChanged();
+                        listView.invalidateViews();
+                        listView.refreshDrawableState();
+                    }
+                }
             }
         });
 
